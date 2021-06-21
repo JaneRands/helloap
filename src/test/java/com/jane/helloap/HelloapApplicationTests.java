86e1957ecab0327abd5a614cb8ac169c832a1795
JaneRands <79332003+JaneRands@users.noreply.github.com>
2021-06-20 21:58:08 -0700

    Add files via upload
    spring.io generated with spring web dependency as a java 8 jar and spring boot 2.5.1

---
+++ src/test/java/com/jane/helloap/HelloapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.helloap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
